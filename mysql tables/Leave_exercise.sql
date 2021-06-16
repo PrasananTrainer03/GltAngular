5ecb9d866a63f13aea0b306078592b858cf99dcb
Prasanna <prassup.dotnet@gmail.com>
2021-06-16 14:32:51 +0530

    code

---
+++ mysql tables/Leave_exercise.sql
@@ -0,0 +1,26 @@
+-- Display all records from EMploy table
+
+-- Display all recods from Employ table who joined in year 2021
+
+-- Display all records from Employ table who are not joined in 2020
+
+-- Display all records from Employ table who belongs to UI
+
+-- Display total no.of employees in Java Department
+
+-- Display max salary of employees who belongs to Java job 
+
+-- Display total salary paid to all employees 
+
+-- Display salary paid to employees based on job
+
+-- Calcualte noofDays and show me (leaveEndDate-leaveStartDate) 
+
+-- Display employ names who are applied for leave
+
+-- Display employee names who are not applied for leave 
+
+-- Display employee names who applied for more than 1 leave 
+
+-- Display all records whose LeaveRecords are wrong means (leaveStartDate
+-- > leaveEndDate)
