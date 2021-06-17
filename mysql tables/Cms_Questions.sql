811e3873693d6fd2557f32975a1a4078994742db
Prasanna <prassup.dotnet@gmail.com>
2021-06-17 14:25:36 +0530

    code

---
+++ mysql tables/Cms_Questions.sql
@@ -0,0 +1,51 @@
+-- Display all details of customers 
+
+-- Add all your batch mates records into customer tables
+
+-- Display all records whose customer email is wrong
+   -- as @ symbol missing
+   
+-- Display all records of customer whose mobile no is wrong
+	-- Only Digits and only 10 chars
+    
+-- Display all records from Menu
+
+-- Display all records from Menu whose Speciality is 'VEG'
+
+-- Display all records from Menu whose price < 700 
+
+-- Display all records from Menu whose Price > 700 and Type 'NON-VEG'
+
+--  Dislay all records from Menu in which the word 'CHICKEN' exists
+
+-- Add 6 more records to Menu table 
+
+-- Display all records from Vendor 
+
+-- Display all records from Wallet
+
+-- Display CustomerID, CustomerName,Customeruser,
+ -- walletID, WalletType,WalletAmount from 
+ -- Customer and Wallet Tables 
+ 
+ -- Above Query apply INNER and OUTER Joins 
+ 
+ -- Display all records from Customer who 
+ -- dont have any wallet
+ 
+ -- Display all customer and Wallet information 
+ -- for whose customer Wallet Type < 4000 
+ 
+ -- Display all wallet Records from Wallet, but 
+ -- if wallet_Type is CREDIT_CARD Then show column ass
+ -- 'Pin Required' else 'No Pin Required'
+ 
+ -- Display value as 'Active' for all vendors 
+ -- except for 'Aarifa', for her give as 'Inactive'
+ 
+ -- Display all vendors whose userName does not contains
+ -- any digits 
+ 
+ -- For Menu table, display one column as 
+ -- if Speciality is 'VEG' then Show as 'GroundFloor'
+ -- if speciality is 'NON-VEG' then show as 'FirstFloor'
